c37a56625d4c8a62d522206c431929925c455253
Vindie142 <viktor14242@gmail.com>
2023-09-18 21:49:43 +0400

    hello world

---
+++ src/main/java/com/vindie/sunshine_ss/SunshineSsApplication.java
@@ -0,0 +1,13 @@
+package com.vindie.sunshine_ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunshineSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunshineSsApplication.class, args);
+	}
+
+}
