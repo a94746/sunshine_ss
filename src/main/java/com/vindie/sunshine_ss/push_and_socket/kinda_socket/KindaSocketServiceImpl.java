bd2cde5d0f6415798708738e8ddc963319925632
Vindie142 <viktor14242@gmail.com>
2023-10-09 20:14:24 +0400

    liquibase added

--- src/main/java/com/vindie/sunshine_ss/push_and_socket/kinda_socket/KindaSocketServiceImpl.java
+++ src/main/java/com/vindie/sunshine_ss/push_and_socket/kinda_socket/KindaSocketServiceImpl.java
@@ -1,9 +1,15 @@
 package com.vindie.sunshine_ss.push_and_socket.kinda_socket;
 
 import com.vindie.sunshine_ss.common.record.event.ui.UiEvent;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@Service
+@Slf4j
+@AllArgsConstructor
 public class KindaSocketServiceImpl implements KindaSocketService {
     @Override
     public void sendUpdate(UiEvent uiEvent, Collection<Long> ids) {
--- src/main/java/com/vindie/sunshine_ss/push_and_socket/push/PushServiceImpl.java
+++ src/main/java/com/vindie/sunshine_ss/push_and_socket/push/PushServiceImpl.java
@@ -1,7 +1,14 @@
 package com.vindie.sunshine_ss.push_and_socket.push;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.util.Collection;
 
+@Service
+@Slf4j
+@AllArgsConstructor
 public class PushServiceImpl implements PushService {
     @Override
     public void sendPushs(String pushTitle, String pushText, Collection<Long> accIds) {
